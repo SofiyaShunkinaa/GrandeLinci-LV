08f93d23d7ecd645411acd7e880a0eb7ace020db
SofiyaShunkinaa <112964742+SofiyaShunkinaa@users.noreply.github.com>
2024-02-08 23:07:45 +0300

    added quotation marks

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php session_start();
 define("INDEX", ""); // УСТАНОВКА КОНСТАНТЫ ГЛАВНОГО КОНТРОЛЛЕРА
 
-require_once($_SERVER[DOCUMENT_ROOT]."/cfg/core.php"); // ПОДКЛЮЧЕНИЕ ЯДРА
+require_once($_SERVER['DOCUMENT_ROOT']."/config/core.php"); // ПОДКЛЮЧЕНИЕ ЯДРА
 
 // ПОДКЛЮЧЕНИЕ К БД
 $db = new MyDB();
 $db->connect();
 
 // ГЛАВНЫЙ КОНТРОЛЛЕР
-switch ($_GET[option]) {
+switch ($_GET['option']) {
 case "page":
-include($_SERVER[DOCUMENT_ROOT]."/includes/pages/page.php");
+include($_SERVER['DOCUMENT_ROOT']."/includes/pages/page.php");
 break;
 default:
-include($_SERVER[DOCUMENT_ROOT]."/includes/pages/home.php");
+include($_SERVER['DOCUMENT_ROOT']."/includes/pages/home.php");
 break;
 }
 
-include ($_SERVER[DOCUMENT_ROOT]."/template.php");
+include ($_SERVER['DOCUMENT_ROOT']."/template.php");
 $db->close();
