94d464c026a2de53ad32e78dcbc177375294d26e
Sofiya <sofik_shu2@mail.ru>
2024-02-11 19:29:27 +0300

    separated menu to main and secondary

--- core/layouts/main-menu/default.php
+++ core/layouts/main-menu/default.php
@@ -1,49 +1,91 @@
-<?php
-session_start();
+<header>
+<nav>
+    <div class="app-nav">
+        <div class="secondary-menu">
+            <div class="container">
+                <?php
+                session_start();
 
-$db = new MyDB();
-$db->connect();
+                $db = new MyDB();
+                $db->connect();
 
-$query = "SELECT * FROM main_menu";
-$db->run($query);
+                $query = "SELECT * FROM main_menu";
+                $db->run($query);
 
-$table = array();
-while ($row = $db->fetch()) {
-    $table[] = $row;
-}
+                $table = array();
+                while ($row = $db->fetch()) {
+                    $table[] = $row;
+                }
 
-// $table = array(
-//     array('id' => 1, 'name' => 'Глава 1', 'parent_id' => 0),
-//     array('id' => 2, 'name' => 'Глава 2', 'parent_id' => 0),
-//     array('id' => 3, 'name' => 'Глава 3', 'parent_id' => 0),
-//     array('id' => 4, 'name' => 'Глава 4', 'parent_id' => 0),
-//     array('id' => 5, 'name' => '5	Основы языка PHP. Сценарии	1', 'parent_id' => 1),
-//     array('id' => 6, 'name' => 'Глава 1', 'parent_id' => 1),
-//     array('id' => 7, 'name' => 'Глава 2', 'parent_id' => 2),
-//     array('id' => 8, 'name' => 'Глава 3', 'parent_id' => 2),
-//     array('id' => 9, 'name' => 'Глава 4', 'parent_id' => 3),
-//     array('id' => 10, 'name' => '5	Основы языка PHP. Сценарии	1', 'parent_id' => 3),
-//     array('id' => 11, 'name' => '5	Основы языка PHP. Сценарии	1', 'parent_id' => 10)
-    
-// );
+                $associativeArray = array();
+                foreach ($table as $row) {
+                    $associativeArray[$row['id']] = $row;
+                }
 
-$associativeArray = array();
-foreach ($table as $row) {
-    $associativeArray[$row['id']] = $row;
-}
+                function buildList($items, $parentId = 0) {
+                    $html = "<ul>";
+                    foreach ($items as $item) {
+                        if(preg_match('/secondary/', $item['custom_class'])) {
+                            if ($item['parent_id'] == $parentId) {
+                                $html .= "<li>{$item['name']}";
+                                $html .= buildList($items, $item['id']);
+                                $html .= "</li>";
+                            }
+                        }
+                    }
+                    $html .= "</ul>";
+                    return $html;
+                }
 
-function buildList($items, $parentId = 0) {
-    $html = "<ul>";
-    foreach ($items as $item) {
-        if ($item['parent_id'] == $parentId) {
-            $html .= "<li>{$item['name']}";
-            $html .= buildList($items, $item['id']);
-            $html .= "</li>";
-        }
-    }
-    $html .= "</ul>";
-    return $html;
-}
+                echo buildList($associativeArray);
+                ?>
+            </div>
+        </div>
+    <div class="main-menu">
+        <div class="container">
+            <div class="logo">
+                <a href="#">
+                    GrandeLinci LV
+                </a>
+            </div>
+            <?php
+            session_start();
 
-echo buildList($associativeArray);
-?>
+            $db = new MyDB();
+            $db->connect();
+
+            $query = "SELECT * FROM main_menu";
+            $db->run($query);
+
+            $table = array();
+            while ($row = $db->fetch()) {
+                $table[] = $row;
+            }
+
+            $associativeArray = array();
+            foreach ($table as $row) {
+                $associativeArray[$row['id']] = $row;
+            }
+
+            function buildList1($items, $parentId = 0) {
+                $html = "<ul>";
+                foreach ($items as $item) {
+                    if(!preg_match('/secondary/', $item['custom_class'])) {
+                        if ($item['parent_id'] == $parentId) {
+                            $html .= "<li>{$item['name']}";
+                            $html .= buildList1($items, $item['id']);
+                            $html .= "</li>";
+                        }
+                    }
+                }
+                $html .= "</ul>";
+                return $html;
+            }
+
+            echo buildList1($associativeArray);
+            ?>
+        </div>
+    </div>
+    </div>
+</nav>
+</header>
