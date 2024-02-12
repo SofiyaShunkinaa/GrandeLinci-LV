b0db8e514ef853c332e551f5d13636409ebe26d4
Sofiya <sofik_shu2@mail.ru>
2024-02-12 23:13:24 +0300

    made footer module

--- core/layouts/footer/default.php
+++ core/layouts/footer/default.php
@@ -1,1 +1,14 @@
-<h2>here is footer</h2>
+<footer>
+    <div class="main-footer">
+        <div class="main-footer__logo"><?php echo $Lang['Footer']['logo']; ?></div>
+    </div>
+
+    <div class="secondary-footer">
+        <div class="container">
+            <div class="secondary-footer__content">
+                <span><?php echo $Lang['Footer']['copyright']; ?></span>
+                <span><?php echo $Lang['Footer']['madeby']; ?></span>
+            </div>
+        </div>
+    </div>
+</footer>
