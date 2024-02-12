114d91fab721ef1940bd0c35ebe17a7da5e2cd21
Sofiya <sofik_shu2@mail.ru>
2024-02-12 23:11:06 +0300

    added footer variables to lang files

--- lang/lang_lv.php
+++ lang/lang_lv.php
@@ -1,1 +1,13 @@
 <?php
+$Lang = array(
+    "Footer" 	=> array(
+        "logo" => "Grande<b>Linci</b>",
+        "copyrigth" => "@copyrigth",
+        "madeby" => "Made by Sofiya"
+    ),
+    "header_title" => "Easy php localization",
+    "site_slogan"	=> "Powered by LORD GR8T",
+
+
+);
+?>
