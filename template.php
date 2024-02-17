e6b3344908557734d7f69587a60ad29afa6d20ee
Sofiya <sofik_shu2@mail.ru>
2024-02-17 15:06:19 +0300

    move in: main controller

--- template.php
+++ template.php
@@ -1,5 +1,16 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT']."/core/layouts/main-menu/default.php");
+
+// MAIN CONTROLLER
+switch ($_GET['option']) {
+    case "page":
+        include($_SERVER['DOCUMENT_ROOT']."/core/pages/page.php");
+        break;
+    default:
+        include($_SERVER['DOCUMENT_ROOT']."/core/pages/home.php");
+        break;
+}
+
 require_once($_SERVER['DOCUMENT_ROOT']."/core/layouts/footer/default.php");
 
 
