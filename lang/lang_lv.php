b27e2ee043d60bb66fdd773226c794d334bbc538
Sofiya <sofik_shu2@mail.ru>
2024-02-17 18:19:36 +0300

    add: latvian translation

--- lang/lang_lv.php
+++ lang/lang_lv.php
@@ -5,9 +5,33 @@ $Lang = array(
         "copyright" => "@copyrigth",
         "madeby" => "Made by Sofiya"
     ),
+
+    "Header" => array(
+        "main_menu" => array(
+            1 => "Par mums",
+            2 => "Pieejamie kaķēni",
+            3 => "Mūsu kaķi",
+            4 => "Foto galerija",
+            5 => "Par šķirni",
+            6 => "Kontakti",
+            7 => "Meitenes",
+            8 => "Puiši",
+            9 => "Tests"
+        ),
+        "secondary" => array(
+            10 => "Rīga, Latvija",
+            11 => "+371 25 614 622",
+            13 => "karoleinga@gmail.com",
+            14 => "RU",
+            15 => "LV",
+            16 => "EN",
+            17 => 'lang'
+        )
+
+    ),
+
     "header_title" => "Easy php localization",
     "site_slogan"	=> "Powered by LORD GR8T",
 
 
 );
-?>
