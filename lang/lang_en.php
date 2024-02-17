bd8ecd416d462d9c1f101978a755d7d35d61971a
Sofiya <sofik_shu2@mail.ru>
2024-02-17 14:12:46 +0300

    add: header translation

--- lang/lang_en.php
+++ lang/lang_en.php
@@ -5,9 +5,32 @@ $Lang = array(
         "copyright" => "@copyrigth",
         "madeby" => "Made by Sofiya"
     ),
+
+    "Header" => array(
+        "main_menu" => array(
+            1 => "About us",
+            2 => "Available kittens",
+            3 => "Our cats",
+            4 => "Photo gallery",
+            5 => "About breed",
+            6 => "Contacts",
+            7 => "Girls",
+            8 => "Boys",
+            9 => "Test"
+        ),
+        "secondary" => array(
+            10 => "Riga, Latvia",
+            11 => "+371 25 614 622",
+            13 => "karoleinga@gmail.com",
+            14 => "RU",
+            15 => "LV",
+            16 => "EN"
+        )
+
+    ),
+
     "header_title" => "Easy php localization",
     "site_slogan"	=> "Powered by LORD GR8T",
 
 
 );
-?>
