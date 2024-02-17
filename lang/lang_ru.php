dba8a02f438b817c522f022592ee18a4acf18e53
Sofiya <sofik_shu2@mail.ru>
2024-02-17 14:01:08 +0300

    add: header variables to russsian lang

--- lang/lang_ru.php
+++ lang/lang_ru.php
@@ -5,9 +5,32 @@ $Lang = array(
         "copyright" => "@copyrigth",
         "madeby" => "Made by Sofiya"
     ),
+
+    "Header" => array(
+        "main_menu" => array(
+            1 => "О нас",
+            2 => "Доступные котята",
+            3 => "Наши кошки",
+            4 => "Фотогалерея",
+            5 => "О породе",
+            6 => "Контакты",
+            7 => "Кошки",
+            8 => "Коты",
+            9 => "Тестовый"
+        ),
+        "secondary" => array(
+            10 => "Рига, Латвия",
+            11 => "+371 25 614 622",
+            13 => "karoleinga@gmail.com",
+            14 => "RU",
+            15 => "LV",
+            16 => "EN"
+        )
+
+    ),
+
     "header_title" => "Easy php localization",
     "site_slogan"	=> "Powered by LORD GR8T",
 
 
 );
-?>
