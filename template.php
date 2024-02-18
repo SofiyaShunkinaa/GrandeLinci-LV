bdb827a428d9b8e335c8d3fe9fb9576693472486
Sofiya <sofik_shu2@mail.ru>
2024-02-18 20:06:44 +0300

    add: common part for pages top banner

--- template.php
+++ template.php
@@ -1,4 +1,21 @@
-<h1>Main template</h1>
+<div class="app-head app-head-page">
+    <div class="container">
+        <div class="a-head">
+            <div class="a-head__text">
+                <div class="breadcrumbs">
+                    <p><a href="/?option=home"><?php echo $Lang['breadcrumbs']['main']?></a></p>
+                    <p><a class="breadcrumbs-page" href="<?php echo $_GET['option']?>"><?php echo $Lang['breadcrumbs'][$_GET['option']]['name']?></a></p>
+                </div>
+                <div><h1><?php echo $Lang['Pages'][$_GET['option']]['main']['title']?></h1></div>
+                <div><p><?php echo $Lang['Pages'][$_GET['option']]['main']['subtitle']?></p></div>
+            </div>
+            <div class="a-head__img">
+                <img src="<?php echo $Lang['Pages'][$_GET['option']]['main']['img_path']?>" alt="main_banner">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="app-main page-<?php echo $_GET['option']?>">
 <?php
 
 switch ($_GET['option']) {
@@ -20,7 +37,10 @@ switch ($_GET['option']) {
     case "contacts":
         include($_SERVER['DOCUMENT_ROOT']."/core/pages/contacts.php");
         break;
+    default:
+        include($_SERVER['DOCUMENT_ROOT']."/core/pages/available_kittens.php");
+        break;
 }
-
-
+?>
+</div>
 
