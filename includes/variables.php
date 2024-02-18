fceb317b4e77073fd8fd50601c80794826182cdf
Sofiya <sofik_shu2@mail.ru>
2024-02-18 20:04:55 +0300

    add: file with general vars (photo array)

---
+++ includes/variables.php
@@ -0,0 +1,49 @@
+<?php
+
+$Photos = [
+    "photogallery" => [
+        "sec1" => [
+            0 => "/assets/images/show1",
+            1 => "/assets/images/show2",
+            2 => "/assets/images/show3",
+            3 => "/assets/images/show4",
+            4 => "/assets/images/show5"
+        ],
+        "sec2" => [
+            0 => "/assets/images/kit1",
+            1 => "/assets/images/cat8",
+            2 => "/assets/images/cat9",
+            3 => "/assets/images/kit2",
+            4 => "/assets/images/cat10",
+            5 => "/assets/images/cat11",
+            6 => "/assets/images/cats1",
+            7 => "/assets/images/cat12",
+            8 => "/assets/images/kits1"
+        ],
+        "sec3" => [
+            0 => "/assets/images/cat13",
+            1 => "/assets/images/cat14",
+            2 => "/assets/images/kit3",
+            3 => "/assets/images/kit4",
+            4 => "/assets/images/cats2"
+        ]
+    ],
+    "about_breed" => [
+        0 => "/assets/images/cat16",
+        1 => "/assets/images/cat17",
+        2 => "/assets/images/cat15"
+    ],
+    "about_us" => [
+        "sec" => [
+            0 => "/assets/images/cat8",
+            1 => "/assets/images/cats4",
+            2 => "/assets/images/cat20"
+        ],
+        0 => "/assets/images/cats3"
+    ],
+    "contacts" => [
+        0 => "/assets/images/cat18",
+        1 => "/assets/images/cat10",
+        2 => "/assets/images/cat19"
+    ]
+];
