6298bb2b6f002555c9b32162b0fb3ac10acefd85
Sofiya <sofik_shu2@mail.ru>
2024-02-18 20:42:20 +0300

    add: extra function for linked cards in "contacts"

--- includes/functions.php
+++ includes/functions.php
@@ -8,3 +8,7 @@ function title($title)
 function create_card($icon_path, $title, $text){
     return "<div class='card-container col-3'><img src='{$icon_path}' alt='icon'><h4>{$title}</h4><p>{$text}</p></div>";
 }
+
+function create_linkedcard($icon_path, $title, $text, $link){
+    return "<div class='card-container col-3'><img src='{$icon_path}' alt='icon'><h4>{$title}</h4><p><a href='{$link}'>{$text}</a></p></div>";
+}
