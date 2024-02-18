42932e7400a48e8666d2f92c3681933f39f902ef
Sofiya <sofik_shu2@mail.ru>
2024-02-18 20:09:26 +0300

    fix: array assosiated names

--- lang/lang_en.php
+++ lang/lang_en.php
@@ -30,8 +30,235 @@ $Lang = array(
 
     ),
 
-    "header_title" => "Easy php localization",
-    "site_slogan"	=> "Powered by LORD GR8T",
+    "Pages" => array(
+        'home' => array(
+            "main" => [
+                "title" => "The Maine Coon House",
+                "subtitle" => "Exquisite Beauty, Exceptional Moments!",
+                "img_path" => "/assets/images/home_main.png"
+            ],
+            "section_welcome" => [
+                "title" => "Welcome!",
+                "content" => "Welcome to our cozy cattery, where the Maine Coon is not just a breed but an unforgettable story of love, care, and magnificence."
+            ],
+            "section_events" => [
+                "title" => "Last events",
+                "litter_date" => "Litter 20.09.2023",
+                "parents" => "Parents:",
+                "father" => "Father:",
+                "mother" => "Mother:"
+            ],
+            "section_about" => [
+                "title" => "About us",
+                "article" => [
+                    [
+                        "img_path" => "/assets/images/cat1.png",
+                        "title" => "Health Guarantees",
+                        "content" => "Each cat undergoes regular veterinary examinations, receives high-quality nutrition, and endless attention.Our care for your future four-legged friend begins right here."
+                    ],
+                    [
+                        "img_path" => "/assets/images/cat2.png",
+                        "title" => "Fall in Love with the Personality",
+                        "content" => "Every personality is unique, and we proudly present to you our furry individualists. From affectionate cuddlers to unwavering explorers, we have a Maine Coon for every heart."
+                    ]
+                ]
+            ]
+        ),
+        "available_kittens" => array(
+            "main" => [
+                "title" => "Available kittens",
+                "subtitle" => "Open the doors to the world of little wonders.",
+                "img_path" => "/assets/images/available_main.png"
+            ],
+            "content" => [
+                "title" => "Our Little Stars",
+                "subtitle" => "Get ready to meet your future best friend with a sense of affection and warmth!"
+            ]
+        ),
+        "our_cats" => array(
+            "main" => [
+                "title" => "Our cats",
+                "subtitle" => "Only the Best Purebred Producers",
+                "img_path" => "/assets/images/available_main.png"
+            ],
+            "content" => [
+                "title" => "Elegant Personalities",
+                "subtitle" => "Immerse yourself in a world of unique personalities and stunning beauty crafted by our cats!"
+            ]
+        ),
+        "photogallery" => array(
+            "main" => [
+                "title" => "Photogallery",
+                "subtitle" => "Rays of Happiness in Every Frame!",
+                "img_path" => "/assets/images/photo_main.png"
+            ],
+            "content" => [
+                [
+                    "title" => "Our Winners",
+                    "subtitle" => "Witness moments of glory and victory as our Maine Coons splendidly represented our cattery at exhibitions."
+                ],
+                [
+                    "title" => "Our Pride",
+                    "subtitle" => "Our most enchanting and beautiful cats!"
+                ],
+                [
+                    "title" => "Our Graduates",
+                    "subtitle" => "Immerse yourself in a world of happy family moments, where our graduates become a part of life, bringing joy and warmth to their new owners."
+                ]
+            ]
+        ),
+        "about_breed" => array(
+            "main" => [
+                "title" => "About the Breed",
+                "subtitle" => "Get to know the character and care specifics of your future pet",
+                "img_path" => "/assets/images/available_main.png"
+            ],
+            "content" => [
+                [
+                    "img" => "/assets/images/",
+                    "title" => "Contents",
+                    "list" => [
+                        0 => "Breed History",
+                        1 => "Maine Coon Appearance",
+                        2 => "Maine Coon Temperament",
+                        3 => "Health and Diseases",
+                        4 => "Care and Maintenance"
+                    ]
+                ],
+                [
+                    "img_path" => "/assets/images/cat3.png",
+                    "title" => "Breed History",
+                    "content" => "The name of the breed translates as 'Maine raccoon'. While the first term in this phrase is clear ('Maine' from the name of the U.S. state Maine), the second requires clarification. The unusual striped coloration and fluffy tails of Maine Coons have led breeders to create a legend that the breed was obtained by crossing a member of the cat family with a raccoon.
 
+The legend remains a legend, but the word 'Coon' (short for raccoon) has stuck with the breed."
+                ],
+                [
+                    "img_path" => "/assets/images/cat4.png",
+                    "title" => "Maine Coon Appearance",
+                    "content" => "The appearance of the famous Maine Coon family has been significantly influenced by Maine's climate: surviving in cold snowy continental winters without thick undercoat is very difficult. A wide paw, protected by additional tufts of fur, is also a useful adaptation, helping to glide on icy surfaces without sinking into snow. And impressive size is an invaluable advantage in hunting small game. 
+
+As for modern representatives of the breed, the passion of European breeders for extremization could not help but affect their appearance. Today's Maine Coons have significantly increased in size, their faces have become even more elongated, and their ears have grown considerably."
+                ],
+                [
+                    "img_path" => "/assets/images/cat5.png",
+                    "title" => "Maine Coon Temperament",
+                    "content" => "Maine Coons are unobtrusive cats who highly value their independence. This trait is especially pronounced in females. Animals react favorably to tactile contact, but you won't be able to knead or squeeze them.
+
+The period of maximum physical activity for this breed is in the first five years of life. After reaching this 'respectable' age, cats start to become slightly lazy, preferring passive rest to noisy games."
+                ],
+                [
+                    "img_path" => "/assets/images/cat6.png",
+                    "title" => "Health and Diseases",
+                    "content" => "Among the specific diseases that Coons are susceptible to, breeders highlight abscesses, bald spots, alopecia, and dried-out skin. The causes of these ailments can be excessive use of water procedures, improperly chosen shampoo, and untimely cleaning of the animal's coat."
+                ],
+                [
+                    "img_path" => "/assets/images/cat7.png",
+                    "title" => "Care and Maintenance",
+                    "content" => "The ideal habitat for a Maine Coon is a country house where the animal can roam freely and satisfy its hunting passion. However, breeders claim that with proper care from the owner, cats of this breed can adapt well even in a city apartment. And excursions to meadows and forests can easily be replaced by regular walks on a leash. If you don't have the opportunity to take your pet out often, consider purchasing a tall play complex that can brighten up your pet's everyday life."
+                ]
+            ]
+        ),
+        "about_us" => array(
+            "main" => [
+                "title" => "About Us",
+                "subtitle" => "Welcome to the Grande Linci LV cattery!",
+                "img_path" => "/assets/images/aboutus_main.png"
+            ],
+            "content" => [
+                "title" => "Our Advantages",
+                "adv_cards" => [
+                    [
+                        "icon" => "/assets/images/icons/health.svg",
+                        "title" => "Our Health Guarantee",
+                        "text" => "All our Maine Coons are in excellent health condition to move to a new home."
+                    ],
+                    [
+                        "icon" => "/assets/images/icons/world.svg",
+                        "title" => "Worldwide Delivery",
+                        "text" => "We provide delivery worldwide. Our graduates are already living in 35 countries!"
+                    ],
+                    [
+                        "icon" => "/assets/images/icons/social.svg",
+                        "title" => "Socialization",
+                        "text" => "We pay special attention to socialization, making them wonderful companions."
+                    ],
+                    [
+                        "icon" => "/assets/images/icons/bag.svg",
+                        "title" => "Care Package",
+                        "text" => "You will receive veterinary records, care recommendations, and a starter kit of products."
+                    ]
+                ]
+            ],
+            "section_info" => [
+                "title" => "Grande Linci History",
+                "text" => "An amazing story is maturing in the very heart of our nursery, which began with the love of maine coons. As a team, we have dedicated our passion to this noble breed, striving to create not just a nursery, but a real home for every furry heart. Our path was littered with the joys of birth and success at exhibitions, but the main goal always remained in the center – to create a family for our Maine Coons and their future owners. The history of the nursery is a story of love for these magnificent creatures, which we are happy to share with you!"
+            ],
+            "ads" => "Choose a kitten right now!"
+        ),
+        "contacts" => array(
+            "main" => [
+                "title" => "Contacts",
+                "subtitle" => "Stay Connected!",
+                "img_path" => "/assets/images/contacts_main.png"
+            ],
+            "content" => [
+                "title" => "Our Contacts",
+                "subtitle" => "Choose the most convenient way to contact us",
+                "contact_cards" => [
+                    [
+                        "icon" => "/assets/images/icons/phone.svg",
+                        "title" => "Phone",
+                        "text" => "+371 25 614 622"
+                    ],
+                    [
+                        "icon" => "/assets/images/icons/letter.svg",
+                        "title" => "Email",
+                        "text" => "karoleinga@gmail.com"
+                    ],
+                    [
+                        "icon" => "/assets/images/icons/facebook.svg",
+                        "title" => "Social Media",
+                        "text" => "@GrandeLinciLV"
+                    ],
+                    [
+                        "icon" => "/assets/images/icons/home.svg",
+                        "title" => "Address",
+                        "text" => "Riga, Latvia"
+                    ]
+                ]
+            ]
+        ),
+        404 => array(
+            "content" => "Something went wrong! Try reloading the page or go back to the homepage"
+        )
+    ),
+
+    "Form" => array(
+        "title" => "Become a happy owner of a fluffy pet",
+        "field" => [
+            "name" => "Name*",
+            "email" => "Email*",
+            "phone" => "Phone Number*",
+            "extra_pets" => "Do you have other pets?*",
+            "food" => "What do you plan to feed the pet with?*",
+            "children" => "Are there children in the family?",
+            "chosen_pet" => "Chosen Pet*",
+            "your_q" => "Your Question"
+        ],
+        "check" => [
+            "confidence" => "I give my consent to the processing of personal data. Privacy Policy*",
+            "notifications" => "I want to receive notifications and the latest news from the kennel by email"
+        ]
+    ),
+
+    "breadcrumbs" => [
+        "main" => "Main/",
+        "available_kittens" => [ "name" => " Available kittens", "link" => "/?option=available_kittens" ],
+        "our_cats" => [ "name" => " Our cats", "link" => "/?option=our_cats" ],
+        "photogallery" => [ "name" => " Photo gallery", "link" => "/?option=photogallery" ],
+        "about_breed" => [ "name" => " About breed", "link" => "/?option=about_breed" ],
+        "about_us" => [ "name" => " About us", "link" => "/?option=about_us" ],
+        "contacts" => [ "name" => " Contacts", "link" => "/?option=contacts" ]
+    ]
 
 );
