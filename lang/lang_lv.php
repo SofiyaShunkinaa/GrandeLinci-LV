bc0711a9c25591714460fb2690237857f9fe704c
Sofiya <sofik_shu2@mail.ru>
2024-02-18 20:08:25 +0300

    add: partial latvian translation

--- lang/lang_lv.php
+++ lang/lang_lv.php
@@ -30,8 +30,212 @@ $Lang = array(
 
     ),
 
-    "header_title" => "Easy php localization",
-    "site_slogan"	=> "Powered by LORD GR8T",
+    "Pages" => array(
+        0 => array(
+            "main" => [
+                "title" => "The Maine Coon House",
+                "subtitle" => "Exquisite Beauty, Exceptional Moments!",
+            ],
+            "section_welcome" => [
+                "title" => "Welcome!",
+                "content" => "Welcome to our cozy cattery, where the Maine Coon is not just a breed but an unforgettable story of love, care, and magnificence."
+            ],
+            "section_events" => [
+                "title" => "Last events",
+                "litter_date" => "Litter 20.09.2023",
+                "parents" => "Parents:",
+                "father" => "Father:",
+                "mother" => "Mother:"
+            ],
+            "section_about" => [
+                "title" => "About us",
+                "article" => [
+                    [
+                        "img_path" => "/assets/images/cat1.png",
+                        "title" => "Health Guarantees",
+                        "content" => "Each cat undergoes regular veterinary examinations, receives high-quality nutrition, and endless attention.Our care for your future four-legged friend begins right here."
+                    ],
+                    [
+                        "img_path" => "/assets/images/cat2.png",
+                        "title" => "Fall in Love with the Personality",
+                        "content" => "Every personality is unique, and we proudly present to you our furry individualists. From affectionate cuddlers to unwavering explorers, we have a Maine Coon for every heart."
+                    ]
+                ]
+            ]
+        ),
+        1 => array(
+            "main" => [
+                "title" => "Available kittens",
+                "subtitle" => "Open the doors to the world of little wonders."
+            ],
+            "content" => [
+                "title" => "Our Little Stars",
+                "subtitle" => "Get ready to meet your future best friend with a sense of affection and warmth!"
+            ]
+        ),
+        2 => array(
+            "main" => [
+                "title" => "Our cats",
+                "subtitle" => "Only the Best Purebred Producers"
+            ],
+            "content" => [
+                "title" => "Elegant Personalities",
+                "subtitle" => "Immerse yourself in a world of unique personalities and stunning beauty crafted by our cats!"
+            ]
+        ),
+        3 => array(
+            "main" => [
+                "title" => "Photogallery",
+                "subtitle" => "Rays of Happiness in Every Frame!"
+            ],
+            "content" => [
+                [
+                    "title" => "Our Winners",
+                    "subtitle" => "Witness moments of glory and victory as our Maine Coons splendidly represented our cattery at exhibitions."
+                ],
+                [
+                    "title" => "Our Pride",
+                    "subtitle" => "Our most enchanting and beautiful cats!"
+                ],
+                [
+                    "title" => "Our Graduates",
+                    "subtitle" => "Immerse yourself in a world of happy family moments, where our graduates become a part of life, bringing joy and warmth to their new owners."
+                ]
+            ]
+        ),
+        4 => array(
+            "main" => [
+                "title" => "Par šķirni",
+                "subtitle" => "Iepazīstiet savas nākamās mājdzīvnieka raksturu un kopšanas īpatnības"
+            ],
+            "content" => [
+                [
+                    "img" => "/assets/images/",
+                    "title" => "Saturs",
+                    "list" => [
+                        0 => "Šķirnes vēsture",
+                        1 => "Maine Coon izskats",
+                        2 => "Maine Coon raksturs",
+                        3 => "Veselība un slimības",
+                        4 => "Kopšana un uzturēšana"
+                    ]
+                ],
+                [
+                    "img_path" => "/assets/images/cat3.png",
+                    "title" => "Šķirnes vēsture",
+                    "content" => "Šķirnes nosaukums tulkojams kā 'Maines ērcis'. Kamēr šajā frāzē pirmā daļa ir skaidra ('Maine' no ASV štata nosaukuma Maine), otrai nepieciešama skaidrojums. Neierastais svītrotais krāsojums un pūkainie astes Maine Coonu ir likuši audzētājiem radīt leģendu, ka šķirni ieguva, krustojot kaķu ģimenes locekli ar ērci.
 
+Leģenda paliek par leģendu, bet vārds 'Coon' (saīsināts no ērces) ir palicis pie šķirnes."
+                ],
+                [
+                    "img_path" => "/assets/images/cat4.png",
+                    "title" => "Maine Coon izskats",
+                    "content" => "Slavenā Maine Coonu ģimenes izskatu ir ievērojami ietekmējis Maines klimats: izdzīvot aukstajos sniegotajos kontinentālajos ziemas laikos bez bieza apakšmētelīša ir ļoti grūti. Plata kāja, kas aizsargāta ar papildu pūciņiem, ir noderīga pielāgojums, kas palīdz slīdēt pa ledainiem virsmām, neieslīdot sniegā. Un ievērojamais izmērs ir nenovērtējams priekšrocība medībās uz mazu lauvu. 
 
+Attiecībā uz šīs šķirnes mūsdienu pārstāvjiem Eiropas audzētāju aizrautība ar ekstrēmismu nevarēja nepaiet viņu izskatu. Šodienas Maine Cooni ir ievērojami palielinājušies izmēros, viņu seja ir kļuvusi vēl izstieptāka, un ausis ir būtiski pieaugušas."
+                ],
+                [
+                    "img_path" => "/assets/images/cat5.png",
+                    "title" => "Maine Coon raksturs",
+                    "content" => "Maine Cooni ir neuzbāzīgi kaķi, kuri augstu vērtē savu neatkarību. Šī iezīme ir īpaši izteikta sievietēm. Dzīvnieki labi reaģē uz taustes saziņu, bet jūs nevarēsit tos savelkt vai saspiest.
+
+Šai šķirnei maksimālais fiziskais aktivitātes periods ir pirmajos piecos dzīves gados. Sasniedzot šo 'cienījamo' vecumu, kaķi sāk kļūt nedaudz slinki, priekšroku dodot pasīvai atpūtai pret skaļām spēlēm."
+                ],
+                [
+                    "img_path" => "/assets/images/cat6.png",
+                    "title" => "Veselība un slimības",
+                    "content" => "Starp specifiskajām slimībām, kam pakļauti Cooni, audzētāji izceļ abscesus, plešas, alopēciju un sausas ādas problēmas. Šo slimību cēloņi var būt pārmērīga ūdens procedūru lietošana, nepareizi izvēlēts šampūns un savlaicīga dzīvnieka apģērbu tīrīšana."
+                ],
+                [
+                    "img_path" => "/assets/images/cat7.png",
+                    "title" => "Care and Maintenance",
+                    "content" => "The ideal habitat for a Maine Coon is a country house where the animal can roam freely and satisfy its hunting passion. However, breeders claim that with proper care from the owner, cats of this breed can adapt well even in a city apartment. And excursions to meadows and forests can easily be replaced by regular walks on a leash. If you don't have the opportunity to take your pet out often, consider purchasing a tall play complex that can brighten up your pet's everyday life."
+                ]
+            ]
+        ),
+        5 => array(
+            "main" => [
+                "title" => "About Us",
+                "subtitle" => "Welcome to the Grande Linci LV cattery!"
+            ],
+            "content" => [
+                "title" => "Our Advantages",
+                "adv_cards" => [
+                    [
+                        "icon" => "health.svg",
+                        "title" => "Our Health Guarantee",
+                        "text" => "All our Maine Coons are in excellent health condition to move to a new home."
+                    ],
+                    [
+                        "icon" => "world.svg",
+                        "title" => "Worldwide Delivery",
+                        "text" => "We provide delivery worldwide. Our graduates are already living in 35 countries!"
+                    ],
+                    [
+                        "icon" => "social.svg",
+                        "title" => "Socialization",
+                        "text" => "We pay special attention to socialization, making them wonderful companions."
+                    ],
+                    [
+                        "icon" => "bag.svg",
+                        "title" => "Care Package",
+                        "text" => "You will receive veterinary records, care recommendations, and a starter kit of products."
+                    ]
+                ]
+            ]
+        ),
+        6 => array(
+            "main" => [
+                "title" => "Kontakti",
+                "subtitle" => "Palieciet savienotiem!"
+            ],
+            "content" => [
+                "title" => "Mūsu Kontakti",
+                "subtitle" => "Izvēlieties ērtāko veidu, kā sazināties ar mums",
+                "contact_cards" => [
+                    [
+                        "icon" => "phone.svg",
+                        "title" => "Tālrunis",
+                        "text" => "+371 25 614 622"
+                    ],
+                    [
+                        "icon" => "letter.svg",
+                        "title" => "E-pasts",
+                        "text" => "karoleinga@gmail.com"
+                    ],
+                    [
+                        "icon" => "facebook.svg",
+                        "title" => "Sociālie Tīkli",
+                        "text" => "@GrandeLinciLV"
+                    ],
+                    [
+                        "icon" => "home.svg",
+                        "title" => "Adrese",
+                        "text" => "Rīga, Latvija"
+                    ]
+                ]
+            ]
+        ),
+        404 => array(
+            "content" => "Kaut kas nogāja greizi! Mēģiniet atkārtoti ielādēt lapu vai doties atpakaļ uz sākumlapu"
+        )
+        ),
+
+        "Form" => array(
+            "title" => "Kļūstiet par laimīgu pūkainā mājdzīvnieka īpašnieku",
+            "field" => [
+                "name" => "Vārds*",
+                "email" => "E-pasts*",
+                "phone" => "Telefona Numurs*",
+                "extra_pets" => "Vai jums ir citi mājdzīvnieki?*",
+                "food" => "Ar ko plānojat barot mājdzīvnieku?*",
+                "children" => "Vai ģimenē ir bērni?",
+                "chosen_pet" => "Izvēlētais Mājdzīvnieks*",
+                "your_q" => "Jūsu Jautājums"
+            ],
+            "check" => [
+                "confidence" => "Es piekrītu savu personisko datu apstrādei. Konfidencialitātes politika*",
+                "notifications" => "Es vēlos saņemt paziņojumus un jaunākās ziņas no patversmes pa e-pastu"
+            ]
+        )
 );
