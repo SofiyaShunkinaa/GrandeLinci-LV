082a1fa2d662b14e5e1e49ab289ed53c4777ea9c
Sofiya <sofik_shu2@mail.ru>
2024-02-19 14:11:09 +0300

    add: extra function to return h3 title

--- includes/functions.php
+++ includes/functions.php
@@ -5,6 +5,11 @@ function title($title)
     return "<div class='title-container'><h2>{$title}</h2><div class='title-rect'></div></div>";
 }
 
+function title_h3($title, $href)
+{
+    return "<div class='title-container' id='{$href}'><h3>{$title}</h3><div class='title-rect title-rect-h3'></div></div>";
+}
+
 function create_card($icon_path, $title, $text){
     return "<div class='card-container col-3'><img src='{$icon_path}' alt='icon'><h4>{$title}</h4><p>{$text}</p></div>";
 }
