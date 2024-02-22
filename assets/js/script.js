6aa2d1d9eecfd1b5699472e7451b73c78196a394
Sofiya <sofik_shu2@mail.ru>
2024-02-22 23:48:23 +0300

    add: functions to work with form data

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,21 +23,64 @@ splide.mount();
 
 //POPUP
 $(document).ready(function(){
-    //Скрыть PopUp при загрузке страницы
     PopUpHide();
 });
-//Функция отображения PopUp
+
 function PopUpShow(value){
     $('html, body').scrollTop(0);
     $("#popup1").show();
     $("#catSelect").val(value);
 }
 
-//Функция скрытия PopUp
 function PopUpHide(){
     $("#popup1").hide();
 }
+
 function openPopup(value) {
-    var url = "/core/layouts/pop-up/form.php" ; // Передача значения через URL параметр
+    var url = "/core/layouts/pop-up/form.php" ;
     window.open(url, "Popup", "width=400, height=400");
 }
+
+// BUTTON SUBMIT
+$('#submit-btn').click(function (e){
+    e.preventDefault();
+
+    let name = $('input[name="name"]').val(),
+        email = $('input[name="email"]').val(),
+        phone = $('input[name="phone_number"]').val(),
+        q1 = $('input[name="q1"]').val(),
+        q2 = $('input[name="q2"]').val(),
+        q3 = $('input[name="q3"]').val(),
+        q4 = $('input[name="q4"]').val(),
+        kit_id = $('#catSelect').val(),
+        submit1 = $('input[name="submit1"]').val(),
+        submit2 = $('input[name="submit2"]').val();
+
+    let formData = new FormData();
+    formData.append('name', name);
+    formData.append('email', email);
+    formData.append('phone', phone);
+    formData.append('q1', q1);
+    formData.append('q2', q2);
+    formData.append('q3', q3);
+    formData.append('q4', q4);
+    formData.append('kit_id', kit_id);
+    formData.append('submit1', submit1);
+    formData.append('submit2', submit2);
+
+    $.ajax({
+        url: 'core/layouts/pop-up/get_data.php',
+        type: 'POST',
+        dataType: 'json',
+        data: formData,
+        success (data) {
+
+            if(data.status){
+                console.log("nice!")
+            }else{
+                console.log(data.field);
+                $('.error').removeClass('none').text(data.field);
+            }
+        }
+    })
+});
