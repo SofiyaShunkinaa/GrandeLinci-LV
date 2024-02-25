163302dc1d87247ca8815d3cdc75a16084cb00db
Sofiya <sofik_shu2@mail.ru>
2024-02-25 21:01:27 +0300

    test: add_cat function (success)

--- includes/add_items.php
+++ includes/add_items.php
@@ -1,3 +1,5 @@
 <?php
 
 //$db->add_mainmenu_item("testing", "#", 1, '', "тест", 'test', 'test', $_SESSION['NowLang']);
+
+//$db->add_cat('test_adding', '2024-02-02', 1, 1, 1, '/assets/cats/cat1.png', "тестирование", "..", "..", "..", "test adding", "..", "..", "..", "test adding", "..", "..", "..", $_SESSION['NowLang']);
