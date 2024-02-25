994839db10c4f056c2cd1f0b0d3ace1ca6b28e50
Sofiya <sofik_shu2@mail.ru>
2024-02-25 21:21:11 +0300

    test: add_kitten function

--- includes/add_items.php
+++ includes/add_items.php
@@ -3,3 +3,5 @@
 //$db->add_mainmenu_item("testing", "#", 1, '', "тест", 'test', 'test', $_SESSION['NowLang']);
 
 //$db->add_cat('test_adding', '2024-02-02', 1, 1, 1, '/assets/cats/cat1.png', "тестирование", "..", "..", "..", "test adding", "..", "..", "..", "test adding", "..", "..", "..", $_SESSION['NowLang']);
+
+//$db->add_kitten("test", 500, 1, 1, 1, 1, 1, 1, "/assets/images/kittens/kit.png", "тестовый", '..', "..", "testing", "..", "..", "testing", "..", "..", $_SESSION['NowLang']);
