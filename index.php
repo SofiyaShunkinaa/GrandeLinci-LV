546a0d127049e1cb6d5f8094c926592f7f34cc81
Sofiya <sofik_shu2@mail.ru>
2024-03-04 22:34:42 +0300

    change: getting data, deleted useless vars

--- index.php
+++ index.php
@@ -59,43 +59,19 @@ include($_SERVER['DOCUMENT_ROOT']."/template.php");
 require_once($_SERVER['DOCUMENT_ROOT']."/core/layouts/footer/default.php");
 
 try {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $q1 = $_POST['q1'];
-    $q2 = $_POST['q2'];
-    $q3 = isset($_POST['q3']) ? $_POST['q3'] : '';
-    $q4 = isset($_POST['q4']) ? $_POST['q4'] : '';
-    $kit_id = isset($_POST['kit_id']) ? $_POST['kit_id'] : 1;
+    $name = $_POST['name'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $phone = $_POST['phone'] ?? '';
+    $q1 = $_POST['q1'] ?? '';
+    $q2 = $_POST['q2'] ?? '';
+    $q3 = $_POST['q3'] ?? '';
+    $q4 = $_POST['q4'] ?? '';
+    $kit_id = $_POST['kit_id'] ?? 1;
 
 
     $query = "INSERT INTO `booking_request` (`user_name`, `email`, `phone_number`, `id_kitten`, `question1`, `question2`, `question3`, `question4`) VALUES ('$name', '$email', '$phone', $kit_id, '$q1', '$q2', '$q3', '$q4')";
     $result = $db->run($query);
 
-    if ($result) {
-        $response = [
-            "status" => true,
-            "mes" => "cool"
-        ];
-        echo "nice";
-    } else {
-        $response = [
-            "status" => false,
-            "mes" => "not cool"
-        ];
-        echo "not nice";
-    }
+} catch (Exception $e) {}
 
-    echo json_encode($response);
-
-} catch (Exception $e) {
-    $response = [
-        "status" => false,
-        "mes" => "Exception: " . $e->getMessage()
-    ];
-
-    echo json_encode($response);
-}
-
-$db->close();
 
