3b65abaa594b2f371c0c0055617e92c8c2ff16ca
Sofiya <sofik_shu2@mail.ru>
2024-03-07 23:58:25 +0300

    fix: form error names

--- includes/add_items.php
+++ includes/add_items.php
@@ -6,32 +6,32 @@
 
 //$db->add_kitten("test", 500, 1, 1, 1, 1, 1, 1, "/assets/images/kittens/kit.png", "тестовый", '..', "..", "testing", "..", "..", "testing", "..", "..", $_SESSION['NowLang']);
 
-include("lang/lang_ru.php");
-$Lang['Form']['errors']['name'] = "Неверное имя";
-$Lang['Form']['errors']['name'] = "Неверный email";
-$Lang['Form']['errors']['name'] = "Неверный номер телефона";
-$Lang['Form']['errors']['name'] = "Пустое поле о других животных";
-$Lang['Form']['errors']['name'] = "Пустое поле о питании";
-$serializedArray = serialize($Lang);
-file_put_contents('lang/lang_ru.txt', $serializedArray);
-
-include("lang/lang_en.php");
-$Lang['Form']['errors']['name'] = "Incorrect name";
-$Lang['Form']['errors']['name'] = "Incorrect email";
-$Lang['Form']['errors']['name'] = "Incorrct phone";
-$Lang['Form']['errors']['name'] = "Empty field about other pets";
-$Lang['Form']['errors']['name'] = "Empty field about food";
-$serializedArray = serialize($Lang);
-file_put_contents('lang/lang_en.txt', $serializedArray);
-
-include("lang/lang_lv.php");
-$Lang['Form']['errors']['name'] = "Nepareizs vārds";
-$Lang['Form']['errors']['email'] = "Nepareizs e-pasts";
-$Lang['Form']['errors']['phone'] = "Nepareizs tālrunis";
-$Lang['Form']['errors']['other_pets'] = "Tukšs lauks par citiem mājdzīvniekiem";
-$Lang['Form']['errors']['food'] = "Tukšs lauks par ēdienu";
-
-$serializedArray = serialize($Lang);
-file_put_contents('lang/lang_lv.txt', $serializedArray);
-
-include("lang/lang_ru.php");
+//include("lang/lang_ru.php");
+//$Lang['Form']['errors']['name'] = "Неверное имя";
+//$Lang['Form']['errors']['email'] = "Неверный email";
+//$Lang['Form']['errors']['phone'] = "Неверный номер телефона";
+//$Lang['Form']['errors']['q1'] = "Пустое поле о других животных";
+//$Lang['Form']['errors']['q2'] = "Пустое поле о питании";
+//$serializedArray = serialize($Lang);
+//file_put_contents('lang/lang_ru.txt', $serializedArray);
+//
+//include("lang/lang_en.php");
+//$Lang['Form']['errors']['name'] = "Incorrect name";
+//$Lang['Form']['errors']['email'] = "Incorrect email";
+//$Lang['Form']['errors']['phone'] = "Incorrct phone";
+//$Lang['Form']['errors']['q1'] = "Empty field about other pets";
+//$Lang['Form']['errors']['q2'] = "Empty field about food";
+//$serializedArray = serialize($Lang);
+//file_put_contents('lang/lang_en.txt', $serializedArray);
+//
+//include("lang/lang_lv.php");
+//$Lang['Form']['errors']['name'] = "Nepareizs vārds";
+//$Lang['Form']['errors']['email'] = "Nepareizs e-pasts";
+//$Lang['Form']['errors']['phone'] = "Nepareizs tālrunis";
+//$Lang['Form']['errors']['q1'] = "Tukšs lauks par citiem mājdzīvniekiem";
+//$Lang['Form']['errors']['q2'] = "Tukšs lauks par ēdienu";
+//
+//$serializedArray = serialize($Lang);
+//file_put_contents('lang/lang_lv.txt', $serializedArray);
+//
+//include("lang/lang_ru.php");
